cadbc178accbbe9f9fb85844972907a23dcb6da0
cipherf <amir.foroughi@aut.ac.ir>
2024-02-24 17:26:58 +0330

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int[] arr = {1, 2, 3};
+        boolean f = false;
+        if (f){
+
+        }else{
+
+        }
+
+        // loop
+        //while
+//        while(false){
+//
+//        }
+
+//        do{
+//
+//        }while(true);
+
+        // for
+
+
+//        for(int k=0 ; k < 3; k++){
+//            printf("%d", arr[k]);
+//        }
+        //for each
+
+//        for(int el : arr){
+//
+//        }
+
+        //print
+//        printf("hello world %d", integer);
+        int integer = 100;
+//        System.out.println("hello world " + integer);
+//        System.out.print("hello world " + integer);
+//        System.out.printf("hello world %d", integer);
+//        System.out.println()
+
+        // input
+        Scanner reader = new Scanner(System.in);
+        String str1 = reader.next();
+        String str2 = reader.nextLine();
+        int int3 = reader.nextInt();
+        // String
+        String str4 = "hello world";
+
+
+    }
+
+
+}
